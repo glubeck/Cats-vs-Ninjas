4c1119842021897b1d20f3395747e877971bde4d
glubeck <gdlubeck@gmail.com>
2016-03-26 14:02:46 -0600

    stuff

---
+++ Assets/Scripts/DeathTrigger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathTrigger : MonoBehaviour {
+
+	private Player player1;
+	private Player2 player2;
+
+	void Start() {
+		player1 = GameObject.FindGameObjectWithTag ("Player1").GetComponent<Player>();
+		player2 = GameObject.FindGameObjectWithTag ("Player2").GetComponent<Player2>();
+	}
+
+	void OnTriggerEnter2D(Collider2D col) {
+
+		if (col.CompareTag ("Player1DeathCheck")) {
+			player1.Die ();
+		} else if (col.CompareTag ("Player2DeathCheck")) {
+			player2.Die ();
+		}
+	}
+}
---
+++ Assets/Scripts/Player2.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player2 : MonoBehaviour {
+
+	public float maxSpeed = 3;
+	public float speed = 50f;
+	public float jumpPower = 200f;
+
+	public bool grounded;
+
+	private Rigidbody2D rb2d;
+	private Animator anim;
+
+	// Stats
+	int curHealth = 0;
+	int lives = 3;
+
+	void Start () {
+		rb2d = gameObject.GetComponent<Rigidbody2D> ();
+		anim = gameObject.GetComponent<Animator> ();
+	}
+
+
+	void Update() {
+
+		anim.SetBool ("Grounded", grounded);
+		anim.SetFloat ("Speed", Mathf.Abs(Input.GetAxis("Horizontal2")));
+
+		if (Input.GetAxis ("Horizontal2") < -0.1f) {
+			transform.localScale = new Vector3 (-1, 1, 1);
+		}
+
+		if (Input.GetAxis ("Horizontal2") > 0.1f) {
+			transform.localScale = new Vector3 (1, 1, 1);
+		}
+
+		if (Input.GetButtonDown ("Jump2")) {
+			rb2d.AddForce (Vector2.up * jumpPower);
+		}
+	}
+
+	void FixedUpdate () {
+
+		float h = Input.GetAxis ("Horizontal2");
+
+		rb2d.AddForce ((Vector2.right * speed) * h);
+
+		//limits speed
+		if (rb2d.velocity.x > maxSpeed) {
+			rb2d.velocity = new Vector2 (maxSpeed, rb2d.velocity.y);
+		}
+
+		if (rb2d.velocity.x < -maxSpeed) {
+			rb2d.velocity = new Vector2 (-maxSpeed, rb2d.velocity.y);
+		}
+	}
+
+	public void Die() {
+		rb2d.velocity = new Vector3 (0, 0, 0);
+		transform.position = new Vector3 (1, 1, 1);
+		curHealth = 0;
+		lives -= 1;
+	}
+}
